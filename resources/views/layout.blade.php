fc01a5286a6790a467e62298ac93e6a0e8443b64
ibrandiaye <ibra789ndiaye@gmail.com>
2024-12-09 14:40:02 +0000

    first commit 6

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -390,10 +390,10 @@
 <script src="{!! asset('assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') !!}"></script>
 <!-- AdminLTE App -->
 <script src="{!! asset('assets/dist/js/adminlte.js') !!}"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+{{-- <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
 <script src="{!! asset('assets/dist/js/pages/dashboard.js') !!}"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="{!! asset('assets/dist/js/demo.js') !!}"></script>
+<script src="{!! asset('assets/dist/js/demo.js') !!}"></script> --}}
 
 
 <!-- DataTables -->
